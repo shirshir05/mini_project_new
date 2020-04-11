cf77e6d6c2061e4b0abaa393b7d899c96a54b5db
Shir Cohen <shir.cohen1@motorolasolutions.com>
2020-04-11 08:47:34 +0300

    add format test

---
+++ Test/Busnies_Servic/Service_Layer/LogAndExitControllerTest.java
@@ -0,0 +1,185 @@
+package Busnies_Servic.Service_Layer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+import static org.junit.Assert.*;
+
+public class LogAndExitControllerTest {
+
+
+    /**
+     * Test - 8
+     */
+    @RunWith(Parameterized.class)
+    public static class checkEmail{
+        //parameter
+
+
+        @Parameterized.Parameters
+        public static Collection<Object[]> data() {
+            return Arrays.asList(new Object[][]{
+
+
+            });
+        }
+        public checkEmail() {
+            //parameter
+        }
+        @Test
+        public void checkEmailTest() {
+
+        }
+
+
+    }//checkEmail
+
+    /**
+     * Test - 9
+     */
+    @RunWith(Parameterized.class)
+    public static class Registration{
+        //parameter
+
+
+        @Parameterized.Parameters
+        public static Collection<Object[]> data() {
+            return Arrays.asList(new Object[][]{
+
+
+            });
+        }
+        public Registration() {
+            //parameter
+        }
+        @Test
+        public void RegistrationTest() {
+
+        }
+
+
+    }//Registration
+
+    /**
+     * Test - 10
+     */
+    @RunWith(Parameterized.class)
+    public static class InputTest{
+        //parameter
+
+
+        @Parameterized.Parameters
+        public static Collection<Object[]> data() {
+            return Arrays.asList(new Object[][]{
+
+
+            });
+        }
+        public InputTest() {
+            //parameter
+        }
+        @Test
+        public void InputTestTest() {
+
+        }
+
+
+    }//InputTest
+
+    /**
+     * Test - 11
+     */
+    @RunWith(Parameterized.class)
+    public static class Login{
+        //parameter
+
+
+        @Parameterized.Parameters
+        public static Collection<Object[]> data() {
+            return Arrays.asList(new Object[][]{
+
+
+            });
+        }
+        public Login() {
+            //parameter
+        }
+        @Test
+        public void LoginTest() {
+
+        }
+
+
+    }//Login
+
+
+    /**
+     * Test - 12
+     */
+    @RunWith(Parameterized.class)
+    public static class Exit{
+        //parameter
+
+
+        @Parameterized.Parameters
+        public static Collection<Object[]> data() {
+            return Arrays.asList(new Object[][]{
+
+
+            });
+        }
+        public Exit() {
+            //parameter
+        }
+        @Test
+        public void ExitTest() {
+
+        }
+
+
+    }//Exit
+
+    /**
+     * Test - 13
+     */
+    @RunWith(Parameterized.class)
+    public static class RemoveSubscription{
+        //parameter
+
+
+        @Parameterized.Parameters
+        public static Collection<Object[]> data() {
+            return Arrays.asList(new Object[][]{
+
+
+            });
+        }
+        public RemoveSubscription() {
+            //parameter
+        }
+        @Test
+        public void RemoveSubscriptionTest() {
+
+        }
+
+
+    }//RemoveSubscription
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
