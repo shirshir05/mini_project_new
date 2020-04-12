64299fc2d0d33c7018cc030602a3d2e767a69665
school & work <michalez@post.bgu.ac.il>
2020-04-12 14:10:11 +0300

    michal page permissions

--- src/Busnies_Servic/Business_Layer/Trace/PersonalPage.java
+++ src/Busnies_Servic/Business_Layer/Trace/PersonalPage.java
@@ -1,13 +1,45 @@
 package Busnies_Servic.Business_Layer.Trace;
+import Busnies_Servic.Business_Layer.UserManagement.Subscription;
+
+import java.util.HashSet;
 import java.util.Observable;
 
 
 
 public class PersonalPage extends Observable {
+
     protected String name;
-    public PersonalPage(String page_name){
-        name=page_name;
+    protected HashSet<String> perrmissionToEdit;
+    protected String pageOwner;
+
+
+    public PersonalPage(String subject_name){
+        name=subject_name;
+        perrmissionToEdit = new HashSet<>();
     }
+
+    public void addPerrmissionToEdit(String user){
+        perrmissionToEdit.add(user);
+    }
+
+    public void addPageOwner(String user){
+        pageOwner = user;
+        perrmissionToEdit.add(user);
+    }
+
+    public boolean chackPerrmissionToEdit(String user){
+        return perrmissionToEdit.contains(user);
+    }
+
+    public boolean removePerrmissionToEdit(String user){
+        boolean ans = false;
+        if(!user.equals(pageOwner)) {
+            perrmissionToEdit.remove(user);
+            ans = true;
+        }
+        return ans;
+    }
+
     /*
     כאשר רוצים לשנות משהו בתוך הפונקציה נקרא לשיטות:
 public change(){
